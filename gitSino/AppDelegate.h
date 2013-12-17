510d01771a077c7f4ae5cda6f927ffebb24f2d38
cyanios <cyan@sinotype.com.cn>
2013-12-17 14:31:59 -0800

    Initial Commit

---
+++ gitSino/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gitSino
+//
+//  Created by ios on 13-12-17.
+//  Copyright (c) 2013年 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
